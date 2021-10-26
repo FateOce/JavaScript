2f941f581398bcc189c3fd20d5648d33e00c260a
FateOce <90393820+FateOce@users.noreply.github.com>
2021-10-26 18:24:15 +1100

    New folder
    Recent variable folder added

---
+++ Variables/index.js
@@ -0,0 +1,17 @@
+var age = 30; 
+var firstName = "Christine ";
+var lastName = "Dee";
+var online = true;
+var whatever = null;
+
+age = age +1 ;
+var fullName = firstName + "" +lastName
+
+
+
+
+console.log("your age is " + age)
+console.log("Are you online?:",online)
+console.log("Hello",firstName)
+console.log( age)
+
