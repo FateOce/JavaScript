c66a948fd519d735619a6c5bdfdaddd9c1a134e5
FateOce <90393820+FateOce@users.noreply.github.com>
2021-10-26 20:56:47 +1100

    Complete

--- Arithmetic expressions/index.js
+++ Arithmetic expressions/index.js
@@ -1,3 +1,22 @@
 var friends = 10
-friends = friends + 1;
+//friends = friends + 1;
+//friends = friends - 1;
+//friends = friends * 2;
+//friends = friends / 2;
+//friends = friends ** 2;
+
+//friends += 1;
+//friends -= 1;
+//friends *= 2;
+//friends /= 2;
+//friends **= 2;
+
+
+friends = (10 + 2) / 2;
+
+
 console.log(friends)
+
+//var remainingFriends = friends % 3;
+
+//console.log(remainingFriends)
