db6806d7bc10704fc247afb078b9a7c4183b5c24
FateOce <90393820+FateOce@users.noreply.github.com>
2021-10-30 19:28:20 +1100

    Complete

---
+++ If statements/boolean.js
@@ -0,0 +1,8 @@
+var online = true;
+
+if(online == true){
+   console.log("Christine is online"); 
+}
+else{
+    console.log("Christine is offline");
+}
---
+++ If statements/index.js
@@ -0,0 +1,14 @@
+var age = -1;
+
+if(age >=65){
+    console.log("You are a senior");
+}
+else if(age >=18){
+    console.log("You are an adult!"); 
+}
+else if(age < 0){
+    console.log("You haven't been born yet!"); 
+}
+else{
+    console.log("You are a child!");
+}
