9bc8276d31c6bedc4aabef262090fdf3a44e3615
FateOce <90393820+FateOce@users.noreply.github.com>
2021-10-30 20:09:49 +1100

    Complete

---
+++ Switches/index.js
@@ -0,0 +1,17 @@
+var grade = "F";
+
+if(grade == "A"){
+    console.log("You did great!");
+}
+else if(grade == "B"){
+    console.log("You did good!")
+}
+else if(grade == "C"){
+    console.log("You did okay")
+}
+else if(grade == "D"){
+    console.log("You... did not do that well")
+}
+else if(grade == "F"){
+    document.write("YOU FAILED!")
+}
