d0f02b1b0e2a0a308449fa440369c39574756b67
FateOce <90393820+FateOce@users.noreply.github.com>
2021-10-31 19:56:32 +1100

    Complete

---
+++ whileloops/index.js
@@ -0,0 +1,11 @@
+/*var creditNum = window.prompt("YOUR COMPUTER HAS A VIRUS!! ENTER YOUR CREDIT CARD INFO... OR ELSE.");
+
+//while(creditNum == ""){
+    creditNum = window.prompt("YOUR COMPUTER HAS A VIRUS!! ENTER YOUR CREDIT CARD INFO... OR ELSE.");
+}
+console.log("THANK YOU! Your computer no longer has a virus")*/
+
+do{
+    var creditNum = window.prompt("YOUR COMPUTER HAS A VIRUS!! ENTER YOUR CREDIT CARD INFO... OR ELSE.");
+}while(creditNum == "")
+console.log("THANK YOU! Your computer no longer has a virus")
