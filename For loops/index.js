4126e5a05279fedb608b449d09f53027477ce927
FateOce <90393820+FateOce@users.noreply.github.com>
2021-11-05 18:47:53 +1100

    Complete

---
+++ For loops/index.js
@@ -0,0 +1,59 @@
+
+let i = 10;
+
+for(; i > 0; ){
+    console.log(i);
+    i --;
+    
+}
+console.log("HAPPY NEW YEAR!");
+
+// for(let i = 1; i<= 10; i+=2){
+//     console.log(i);
+// }
+
+// for(let jeff = 0; jeff < 5; jeff++){
+//     console.log("loop - i = " + jeff);
+// }
+
+// let k = 0;
+// while (k < 5){
+//     console.log("while - k = " + k);
+//     k++;
+// }
+
+// let j = 0;
+// if (j < 5){
+//     console.log("if - j = " + j);
+//     j++
+// }
+// if (j < 5){
+//     console.log("if - j = " + j);
+//     j++
+// }
+// if (j < 5){
+//     console.log("if - j = " + j);
+//     j++
+// }
+// if (j < 5){
+//     console.log("if - j = " + j);
+//     j++
+// }
+// if (j < 5){
+//     console.log("if - j = " + j);
+//     j++
+// }
+// if (j < 5){
+//     console.log("if - j = " + j);
+//     j++
+// }
+// if (j < 5){
+//     console.log("if - j = " + j);
+//     j++
+// }
+// if (j < 5){
+//     console.log("if - j = " + j);
+//     j++
+// }
+
+
