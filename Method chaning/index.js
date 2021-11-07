f068b8971e7c623950c03214a2a5ea40fa166fff
FateOce <90393820+FateOce@users.noreply.github.com>
2021-11-07 15:17:32 +1100

    Complete

---
+++ Method chaning/index.js
@@ -0,0 +1,9 @@
+let user = "christine";
+
+//let firstLetter = user.charAt(0);
+//firstLetter = firstLetter.toUpperCase();
+// user = firstLetter + user.slice(1);
+
+user = user.charAt(0).toUpperCase() + user.slice(1);
+
+console.log(user);
