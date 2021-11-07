07716af85e992490a21e9cd41edfe261725f4a21
FateOce <90393820+FateOce@users.noreply.github.com>
2021-11-07 16:30:16 +1100

    Complete

---
+++ Var vs let/index.js
@@ -0,0 +1,11 @@
+
+// function doSomething(){
+// for (var i = 1; i <= 3; i++){
+//     console.log(i);
+// }}
+// doSomething();
+// console.log(i);
+
+
+//var name = "Christine";
+let name = "Christine";
