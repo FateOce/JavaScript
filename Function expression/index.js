b2b676859b86c0ca07e0600addb8280d487f9d5f
FateOce <90393820+FateOce@users.noreply.github.com>
2021-11-08 15:03:36 +1100

    Complete

---
+++ Function expression/index.js
@@ -0,0 +1,33 @@
+// sayHello();
+
+// function sayHello(){
+//     console.log("Hello");
+// }
+
+//let greeting = function(){console.log("Hello")};
+//let greeting = function(){document.getElementById("myH1").innerHTML = "Hello!"};
+
+let greeting = "Hello"
+
+outputConsole(greeting);
+
+function outputConsole(x) {
+    console.log(x);
+}
+
+function outputWebsite (x) {
+    document.getElementById("myH1").innerHTML = x;
+}
+
+// let greeting = ()=>{document.getElementById("myH1").innerHTML = "Hello!"};
+// output(greeting);
+
+
+// //output = func => func();
+// function output(func){
+//     func();
+//}
+
+
+// let hi = ()=>{console.log("New")};
+// hi();
--- If statements/index.js
+++ If statements/index.js
@@ -1,14 +1,25 @@
-var age = -1;
+var age =0 ;
 
-if(age >=65){
-    console.log("You are a senior");
-}
-else if(age >=18){
-    console.log("You are an adult!"); 
-}
-else if(age < 0){
-    console.log("You haven't been born yet!"); 
-}
-else{
-    console.log("You are a child!");
-}
+// if(age >=65){
+//     console.log("You are a senior");
+// }
+// else if(age >=18){
+//     console.log("You are an adult!"); 
+// }
+// else if(age < 0){
+//     console.log("You haven't been born yet!"); 
+// }
+// else{
+//     console.log("You are a child!");
+// }
+
+// age > 50
+// //? age >= 65 
+//     ? console.log ("65 or older") 
+//     //: console.log("younger then 65") 
+// : console.log("Below 30"); 
+
+age >= 65 ? console.log("You are a senior")
+            : age >=18 ? console.log("You are an adult!")
+            : age <= 0 ? console.log("You haven't been born yet!")
+            : console.log("You are a child!");
