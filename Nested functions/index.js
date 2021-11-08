fc994adeed39114453f8ebd73f29d679ad0e4ee1
FateOce <90393820+FateOce@users.noreply.github.com>
2021-11-08 19:55:51 +1100

    Complete

--- Nested functions/index.js
+++ Nested functions/index.js
@@ -7,13 +7,21 @@ function login(){
         function displayUserName(){  
         console.log("Hello", userName)
     }
-    
+
         function displayUserInbox(){
+            p = 1;
         console.log("You have",userInbox,"new messages");
     }
     displayUserName();
     displayUserInbox();
 }
 
-
 login();
+console.log(p);
+
+function myFunction() {
+  a = 4;
+} 
+myFunction();
+document.getElementById("demo").innerHTML = a * a;
+
