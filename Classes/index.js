ec02bcf30544f137273cbe0542ddd0688d7ac54c
FateOce <90393820+FateOce@users.noreply.github.com>
2021-11-11 16:22:46 +1100

    Complete

---
+++ Classes/index.js
@@ -0,0 +1,17 @@
+class Car{
+    drive(){
+        console.log("You Drive the car!");
+    }
+    break(){
+        console.log("You step on the breaks!");
+    }
+}
+
+    let car1 = new Car();
+    let car2 = new Car();
+
+    car1.drive();
+    car2.break();
+
+
+
