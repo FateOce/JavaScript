9578b56aeb22b1884724fb3d1596c98392027d3a
FateOce <90393820+FateOce@users.noreply.github.com>
2021-11-17 23:56:37 +1100

    Complete

---
+++ Add HTML elements/index.js
@@ -0,0 +1,17 @@
+const h1 = document.createElement("h1");
+const underline = document.createElement("u");
+underline.innerText = "Hello World!";
+h1.append(underline);
+document.body.append(h1);
+
+const h2 = document.createElement("h2");
+const italic = document.createElement("i");
+italic.innerText = "Sup?";
+h2.append(italic);
+document.body.append(h2);
+
+const p = document.createElement("p");
+const bold = document.createElement("b")
+bold.innerText = "Bye...";
+p.append(bold);
+document.body.append(p);
