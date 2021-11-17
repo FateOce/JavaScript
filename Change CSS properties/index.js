734403203aa50f7e5f901cee9445ee9cc6e929ab
FateOce <90393820+FateOce@users.noreply.github.com>
2021-11-18 01:08:49 +1100

    Complete

---
+++ Change CSS properties/index.js
@@ -0,0 +1,9 @@
+document.querySelector("#myButton").onclick = function(){
+    const h1 = document.querySelector("#myH1");
+    console.log(h1.style);
+    h1.style.backgroundColor = "#00FF00";
+    h1.style.fontFamily = "consolas";
+    h1.style.textAlign = "center";
+    h1.style.border = "4px solid";
+    h1.style.borderColor = "blue";
+}
